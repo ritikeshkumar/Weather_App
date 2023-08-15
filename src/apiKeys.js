c360672b8accbd65b76d23c180bf5204bb504b9b
Ritikesh Kumar <130847778+ritikeshkumar@users.noreply.github.com>
2023-08-15 15:16:37 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "80512f535471a6644819729b65e097e6",
+  key: "{Your secret key}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
