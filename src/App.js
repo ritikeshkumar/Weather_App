22246d76f9d496ca7c544640d34c4fb6cf766316
Ritikesh Kumar <130847778+ritikeshkumar@users.noreply.github.com>
2023-08-15 17:11:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       </div>
       <div className="footer-info">
         Developed by{" "}
-        <a target="_blank" href="https://www.gauravghai.dev/">
+        <a target="_blank" href="myportfolio-w1jv.onrender.com">
           Ritikesh Kumar
         </a>{" "}
       </div>
